cb905f9ab9a843d8099e0629ffe20876974cbd4e
eniworoeva <eniworoeva@yahoo.com>
2022-09-21 11:42:28 +0100

    helper functions

--- helpers/authHelper.go
+++ helpers/authHelper.go
@@ -1,11 +1,24 @@
 package helpers
 
-import "github.com/gin-gonic/gin"
+import (
+	"errors"
+
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string ) (err error) {
+	userType := c.GetString()
+}
 
 func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
 	userType := c.GetString("user_type")
 	uid := c.GetString("uid")
 	err = nil
 
-	if userType == "USER" && uid != userId
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorised to access this resource")
+		return err
+	}
+	err = CheckUserType(c, userType)
+	return err
 }
